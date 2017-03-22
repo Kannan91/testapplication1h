52b86e5d58ec73fbbe8bfb2c2bf53f1530bdd75a
Kannan N <kannann1@in.ibm.com>
2017-03-22 13:22:46 -0500

    commited

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class SimpleServlet
  */
-@WebServlet("/SimpleServlet")
+@WebServlet("/callback")
 public class SimpleServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -20,8 +20,28 @@ public class SimpleServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session=request.getSession(); 
+        PrintWriter out = response.getWriter();  
         response.setContentType("text/html");
-        response.getWriter().print("Hello World!");
+        out.print("Hello World!");
+        Enumeration<?> e = session.getAttributeNames();
+	while (e.hasMoreElements())
+	{
+    String name = (String) e.nextElement();
+
+    // Get the value of the attribute
+    Object value = session.getAttribute(name);
+
+    if (value instanceof Map) {
+        for (Map.Entry<?, ?> entry : ((Map<?, ?>)value).entrySet()) {
+            out.println(entry.getKey() + "=" + entry.getValue());
+        }
+    } else if (value instanceof List) {
+        for (Object element : (List)value) {
+            out.println(element);
+        }
+    }
+}
     }
 
 }
